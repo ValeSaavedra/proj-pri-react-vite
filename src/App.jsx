251c9c9554aa381ce4ab772b2abcccc16156431e
Valeria Saavedra <valeriasaavedra@live.com.ar>
2025-11-27 16:44:16 -0300

    chore:app.css and app.jsx first steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,103 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+//import reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
 import "./App.css";
+import { useApi } from "./hooks/useApi";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
+  /*
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  */
+
+  /*
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      if (!response.ok) {
+        throw new Error("Error en la petición");
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  */
+  const handleReload = () => {
+    const { data, loading, error } = useApi(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+  };
+
+  const { data, loading, error } = useApi(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  if (loading) {
+    return (
+      <div className="app">
+        <h1>Mi App con API jsonplaceholder</h1>
+        <p>Cargando datos...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="app">
+        <h1>Mi App con API jsonplaceholder</h1>
+        <p style={{ color: "red" }}>Error: {error}</p>
+        <button onClick={handleReload}>Reintentar</button>
+      </div>
+    );
+  }
 
   return (
-    <>
-      <p> Mi App</p>
-    </>
+    <div>
+      <h1> Mi App con API jsonplaceholder</h1>
+      <button onClick={handleReload}>Recargar Datos</button>
+
+      <div className="table-cointainer">
+        <h2>Posts de la API:</h2>
+
+        {data && data.length > 0 ? (
+          <table className="data-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Contenido</th>
+                <th>User ID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.slice(0, 10).map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td>{item.body}</td>
+                  <td>{item.userId}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p> No hay datos </p>
+        )}
+      </div>
+    </div>
   );
 }
 
